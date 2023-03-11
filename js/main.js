59559bee3b9281869c544b1a334682695f59bfe9
RachelLindquist <lindqurm@mail.uc.edu>
2023-03-11 14:30:31 -0500

    working color change buttons

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ d3.tsv('data/Cincy311_2022_final.tsv')
 .then(data => {
     console.log(data[0]);
     console.log(data.length);
+    d3.select("#callT").classed('inactive', true);
 
     var data = data.filter(function(d){
       return d["LAST_TABLE_UPDATE"].length >= 1;
@@ -29,7 +30,7 @@ d3.tsv('data/Cincy311_2022_final.tsv')
     let ptList = [...new Set(data.map(d => d.process))];
     const  ptColors= d3.scaleOrdinal()
     .domain(ptList)
-    .range(d3.quantize(d3.interpolateHcl("#fafa6e", "#2A4858"), ptList.length));
+    .range(d3.quantize(d3.interpolateHcl("#ff0000", "#00ff00"), ptList.length));
 
     //Call Date
     let cdList = [...new Set(data.map(d => d["REQUESTED_DATETIME"]))];
@@ -41,7 +42,7 @@ d3.tsv('data/Cincy311_2022_final.tsv')
     let paList = [...new Set(data.map(d => d["AGENCY_RESPONSIBLE"]))];
     const  paColors= d3.scaleOrdinal()
     .domain(paList)
-    .range(d3.quantize(d3.interpolateHcl("#fafa6e", "#2A4858"), paList.length));
+    .range(d3.quantize(d3.interpolateHcl("#0000ff", "#f0000f"), paList.length));
   
 
     // Initialize chart and then show it
@@ -52,6 +53,33 @@ d3.tsv('data/Cincy311_2022_final.tsv')
 
     //leafletMap.changeColors(cdColors, "REQUESTED_DATETIME");
     //leafletMap.changeMap(3, 2);
+
+
+    d3.selectAll(".color").on('click', function() {
+      // Toggle 'inactive' class
+      //remove inactive from everything
+      d3.select("#callT").classed('inactive', false);
+      d3.select("#procT").classed('inactive', false);
+      d3.select("#callD").classed('inactive', false);
+      d3.select("#pubA").classed('inactive', false);
+      //except this
+      d3.select(this).classed('inactive', !d3.select(this).classed('inactive'));
+      
+      // Filter data accordingly and update vis
+      let callT = document.getElementById("callT");
+      let procT = document.getElementById("procT");
+      let callD = document.getElementById("callD");
+      let pubA = document.getElementById("pubA");
+      if (callT.classList.contains('inactive')){
+        leafletMap.changeColors(ctColors, "SERVICE_CODE");
+      } else if (procT.classList.contains('inactive')){
+        leafletMap.changeColors(ptColors, "process");
+      } else if (callD.classList.contains('inactive')){
+        leafletMap.changeColors(cdColors, "REQUESTED_DATETIME");
+      } else { //pubA
+        leafletMap.changeColors(paColors, "AGENCY_RESPONSIBLE");
+      }
+      });
 
 
   })
